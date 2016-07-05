b37e5962cf2a1737081409820a8e6345d025b905
orbea <ovariegata@yahoo.com>
2016-07-04 17:40:27 -0700

    src/gb: Silence warning in gbGfx.c

--- src/gb/gbGfx.c
+++ src/gb/gbGfx.c
@@ -583,10 +583,12 @@ void gbDrawSprites(bool draw)
           {
             for (int j = x-8; j<300; j++)
               if (j>=0)
+                {
                 if (gbSpeed)
                   gbSpritesTicks[j] += 5;
                 else
                   gbSpritesTicks[j] += 2+(count&1);
+                }
 
           }
           count++;
